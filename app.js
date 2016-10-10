64fb5b1b30c1300c466520f0349f89bce532f71d
Jonathan Aellen <aellen.j@gmail.com>
2016-10-10 12:08:25 -0400

    Loaders and Pre-loaders setup

---
+++ app.js
@@ -0,0 +1,4 @@
+require('./src/js/logger');
+
+document.write('welcome to my app');
+console.log('app loaded');
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,38 @@
-//Bundle the modules into app.bundle.js by running 'webpack' from commmand line
-//Run 'webpack-dev-server' from command line to run a server on localhost:8080/
+module.exports = {
+   entry: ["./src/js/global.js", "./app.js"],
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+   preLoaders: [
+       {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         loader: 'jshint-loader'
 
-var path = require('path');
-var SRC = path.join(__dirname, 'src/');
-var NODE_MODULES = path.join(__dirname, 'node_modules/');
-
-
-var config = {
-     entry: './src/app.js',
-     output: {
-         path: './bin',
-         filename: 'app.bundle.js',
+       }
+   ],
+   loaders: [
+     {
+       test: [/\.js$/, /\.es6$/],
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
      },
-     resolve: {
-     root: [SRC, NODE_MODULES],
-     alias: {
-      'login': path.join(SRC, './js/login'),
-      'nav': path.join(SRC, './js/nav'),
-      'stats': path.join(SRC, './js/stats'),
-      'compare': path.join(SRC, './js/compare'),
-      'favourites': path.join(SRC, './js/favourites'),
-      'account': path.join(SRC, './js/account'),
-      'data-service': path.join(SRC, './js/components/data-service'),
-      'login-component': path.join(SRC, './js/components/login-component'),
-      'switch-component': path.join(SRC, './js/components/switch-component'),
-      'search-component': path.join(SRC, './js/components/search-component'),
-      'star-component': path.join(SRC, './js/components/star-component'),
-      'picture-component': path.join(SRC, './js/components/picture-component'),
-      'stats-main-component': path.join(SRC, './js/components/stats-main-component'),
-      'stats-secondary-component': path.join(SRC, './js/components/stats-secondary-component'),
-      'compare-component': path.join(SRC, './js/components/compare-component'),
-      'carousel-component': path.join(SRC, './js/components/carousel-component'),
-      'compare-main-component': path.join(SRC, './js/components/compare-main-component'),
-      'compare-secondary-component': path.join(SRC, './js/components/compare-secondary-component'),
-      'rankings-component': path.join(SRC, './js/components/rankings-component'),
-      'account-component': path.join(SRC, './js/components/account-component')
-      }
+     {
+       test: /\.json$/,
+       loader: 'json-loader'
      },
-     module: {
-         loaders:
-         [
-           {
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader'
-           },
-           {
-             test: /\.css$/,
-             loaders: ['style', 'css']
-           },
-           {
-             test: /\.scss$/,
-             loaders: ["style", "css", "sass"]
-           },
-           {
-             test: /\.(jpg|png|svg)$/,
-             loader: 'file?name=public/images/[name].[ext]'
-           }
-        ]
+     {
+       test: /\.css$/,
+       loader: 'style-loader!css-loader'
      }
- }
-
-module.exports = config;
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ },
+  watch: true
+}
